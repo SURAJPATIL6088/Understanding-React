011c7128c87e9d83e360441726df60a05bc9d6df
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2024-03-12 23:42:34 +0530

    React Hook : useEffect

---
+++ 08. React Hooks -useEffect (lifecycle, mount, update, cleanup, dependency array)/Practice Mini-Projects/Memory Game (Card Matching)/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Memory Game - Card Matching</h1>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
